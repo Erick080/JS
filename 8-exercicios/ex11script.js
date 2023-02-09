c535f27e4cf9adc2a24cb7a50c71fb88402cf0d0
= <=>
2023-02-08 22:31:45 -0300

    ajustando o modelo

--- 8-exercicios/ex11script.js
+++ 8-exercicios/ex11script.js
@@ -1,1 +1,17 @@
-/* text */
+var corpo = document.body.style.backgroundColor;
+var hora = new Date().getHours();
+var mensagem = document.getElementById("greetings");
+switch(hora){
+    case hora < 7:
+        corpo = "blue";
+        mensagem.innerHTML = "Boa madrugada!";
+        break;
+    case 7 <= hora <=12:
+        corpo = "yellow";
+        mensagem.innerHTML = "Bom dia!";
+        break;
+    case 12 < hora <= 17:
+        corpo = "orange";
+        mensagem.innerHTML = "Boa tarde!";
+        break;
+}
